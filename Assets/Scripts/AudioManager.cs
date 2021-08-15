d022a3468d80ec805887353431bbd4e69dbc7cc2
hoshicameron <hoshicameron@gmail.com>
2021-08-15 14:17:31 +0430

    Third push +Finalizing FPController

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,212 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [Header("Guns Audio Clips")]
+    [SerializeField] private AudioClip shootAudioClip;
+    [SerializeField] private AudioClip aimAudioClip;
+    [SerializeField] private AudioClip riffleReloadAmmoLeftAudioClip;
+    [SerializeField] private AudioClip riffleReloadOutOFAmmoAudioClip;
+    [SerializeField] private AudioClip handGunReloadAmmoLeftAudioClip;
+    [SerializeField] private AudioClip handGunReloadOutOFAmmoAudioClip;
+
+    [Header("Player Audio Clips")]
+    [SerializeField] private AudioClip walkAudioClip;    //the player walk sound
+    [SerializeField] private AudioClip runAudioClip;     //the player run sound
+    [SerializeField] private AudioClip jumpAudioClip;     //the player jump sound
+    [SerializeField] private AudioClip landAudioClip;     //the player jump sound
+
+
+    [Header("Sting Audio Clips")]
+    [SerializeField] private AudioClip[] explosionAudioClips;
+    [SerializeField] private AudioClip[] casingAudioClips;
+    [SerializeField] private AudioClip[] grenadeAudioClips;
+    [SerializeField] private AudioClip[] missAudioClips;
+    [SerializeField] private AudioClip impactAudioClip;
+
+    [Header("Ambient Audio")]
+    [SerializeField] private AudioClip ambientAudioClip;
+    [SerializeField] private AudioClip musicAudioClip;
+
+    [Header("Mixer Groups")]
+    [SerializeField] private AudioMixerGroup ambientGroup; //The ambient mixer group
+    [SerializeField] private AudioMixerGroup musicGroup;  //The music mixer group
+    [SerializeField] private AudioMixerGroup stingGroup;  //The sting mixer group
+    [SerializeField] private AudioMixerGroup playerGroup; //The player mixer group
+    [SerializeField] private AudioMixerGroup effectGroup; //The efect mixer group
+    [SerializeField] private AudioMixerGroup impactGroup; //The efect mixer group
+
+    AudioSource ambientSource;			//Reference to the generated ambient Audio Source
+    AudioSource musicSource;            //Reference to the generated music Audio Source
+    AudioSource stingSource;            //Reference to the generated sting Audio Source
+    AudioSource playerSource;           //Reference to the generated player Audio Source
+    AudioSource effectSource;           //Reference to the generated effect Audio Source
+    AudioSource impactSource;           //Reference to the generated impact Audio Source
+
+    private void Awake()
+    {
+        //If an AudioManager exists and it is not this...
+        if (Instance != null && Instance != this)
+        {
+            //...destroy this. There can be only one AudioManager
+            Destroy(gameObject);
+            return;
+        }
+
+        //This is the current AudioManager and it should persist between scene loads
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+
+        //Generate the Audio Source "channels" for our game's audio
+        ambientSource	= gameObject.AddComponent<AudioSource>() as AudioSource;
+        musicSource		= gameObject.AddComponent<AudioSource>() as AudioSource;
+        stingSource		= gameObject.AddComponent<AudioSource>() as AudioSource;
+        playerSource	= gameObject.AddComponent<AudioSource>() as AudioSource;
+        playerSource	= gameObject.AddComponent<AudioSource>() as AudioSource;
+        effectSource	= gameObject.AddComponent<AudioSource>() as AudioSource;
+        impactSource	= gameObject.AddComponent<AudioSource>() as AudioSource;
+
+
+        //Assign each audio source to its respective mixer group so that it is
+        //routed and controlled by the audio mixer
+        ambientSource.outputAudioMixerGroup = ambientGroup;
+        musicSource.outputAudioMixerGroup	= musicGroup;
+        stingSource.outputAudioMixerGroup	= stingGroup;
+        playerSource.outputAudioMixerGroup	= playerGroup;
+        effectSource.outputAudioMixerGroup	= effectGroup;
+        impactSource.outputAudioMixerGroup = impactGroup;
+
+    }
+
+    public static void PlayWalkAudio()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.playerSource.clip = Instance.walkAudioClip;
+        Instance.playerSource.loop = true;
+        Instance.playerSource.Play();
+    }
+
+    public static void PlayRunAudio()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.playerSource.clip = Instance.runAudioClip;
+        Instance.playerSource.loop = true;
+        Instance.playerSource.Play();
+    }
+
+    public static void PlayShootAudio()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.stingSource.clip = Instance.shootAudioClip;
+        Instance.stingSource.Play();
+    }
+
+    public static void StopPlayerAudio()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.playerSource.Stop();
+    }
+
+    public static void PlayerReloadAudio()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.stingSource.clip = Instance.riffleReloadAmmoLeftAudioClip;
+        Instance.stingSource.Play();
+    }
+
+    public static void PlayExplosionAudioClip()
+    {
+        if(Instance==null)
+            return;
+
+        var index = Random.Range(0, Instance.explosionAudioClips.Length);
+        Instance.effectSource.clip = Instance.explosionAudioClips[index];
+        Instance.effectSource.Play();
+    }
+
+    public static void PlayCasingAudio()
+    {
+        if(Instance==null)
+            return;
+        foreach (var audioClip in Instance.casingAudioClips)
+        {
+            Instance.effectSource.clip = audioClip;
+            Instance.effectSource.Play();
+        }
+    }
+
+    public static void PlayGrenadeExplosionAudio()
+    {
+        if(Instance==null)
+            return;
+
+        var index = Random.Range(0, Instance.grenadeAudioClips.Length);
+        Instance.effectSource.clip = Instance.explosionAudioClips[index];
+        Instance.effectSource.Play();
+    }
+
+    public static void PlayHitImpactAudioClip()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.impactSource.clip = Instance.impactAudioClip;
+        Instance.impactSource.Play();
+    }
+
+    public static void PlayMissAudio()
+    {
+        if(Instance==null)
+            return;
+
+        var index = Random.Range(0, Instance.missAudioClips.Length);
+        Instance.impactSource.clip = Instance.missAudioClips[index];
+        Instance.impactSource.Play();
+
+    }
+
+    public static void PlayJumpAudio()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.playerSource.clip = Instance.jumpAudioClip;
+        Instance.playerSource.loop = false;
+        Instance.playerSource.Play();
+
+    }
+
+    public static void PlayLandAudio()
+    {
+        if(Instance==null)
+            return;
+
+        Instance.playerSource.clip = Instance.landAudioClip;
+        Instance.playerSource.loop = false;
+        Instance.playerSource.Play();
+
+    }
+
+
+
+
+
+
+}
--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -1,16 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
 
 public class FPController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 0.1f;
+    [SerializeField] private float walkSpeed = 0.1f;
+    [SerializeField] private float sprintSpeed = 0.3f;
     [SerializeField] private float xSensivity = 1f;
     [SerializeField] private float ySensivity = 1f;
     [SerializeField] private float jumpForce = 300f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float timeBetweenShoots = 1.0f;
 
     private float minimumX = -90;
     private float maximumX = 90;
+    private float shootTime;
+
+    private float x, z;
+
+    private float moveSpeed;
+    private bool isRunning = false;
+    private bool isJumping = false;
+    private bool isShooting = false;
 
     private Quaternion cameraRot;
     private Quaternion characterRot;
@@ -21,6 +34,14 @@ public class FPController : MonoBehaviour
 
     private bool lockCursor=true;
     private bool cursorIsLocked=true;
+    private static readonly int Aiming = Animator.StringToHash("aiming");
+    private static readonly int ReloadOutOfAmmo = Animator.StringToHash("reloadOutOfAmmo");
+    private static readonly int Shoot = Animator.StringToHash("shoot");
+    private static readonly int Walk = Animator.StringToHash("walk");
+    private static readonly int AimShoot = Animator.StringToHash("aimShoot");
+    private static readonly int Run = Animator.StringToHash("run");
+    private static readonly int KnifeAttack1 = Animator.StringToHash("knifeAttack1");
+    private static readonly int ReloadAmmoLeft = Animator.StringToHash("reloadAmmoLeft");
 
     private void Start()
     {
@@ -29,6 +50,103 @@ public class FPController : MonoBehaviour
         mainCamera=Camera.main;
         cameraRot = mainCamera.transform.localRotation;
         characterRot = transform.localRotation;
+        moveSpeed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        AnimationHandler();
+
+        if (IsGrounded() && isJumping)
+        {
+            isJumping = false;
+            AudioManager.PlayLandAudio();
+        }
+    }
+
+    private void AnimationHandler()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            animator.SetBool(Aiming, !animator.GetBool(Aiming));
+        }
+        if (Input.GetButtonUp("Fire2"))
+        {
+            animator.SetBool(Aiming, !animator.GetBool(Aiming));
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            ShootSequence();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            isShooting = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            animator.SetTrigger(KnifeAttack1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger(ReloadOutOfAmmo);
+        }
+
+        if (Math.Abs(x) > 0 || Math.Abs(z) > 0)
+        {
+            if (isRunning)
+            {
+                if (!animator.GetBool(Run))
+                {
+                    animator.SetBool(Run, true);
+                    AudioManager.PlayRunAudio();
+                }
+
+            } else
+            {
+                if (isJumping)
+                {
+                    if(animator.GetBool(Walk))
+                        animator.SetBool(Walk, false);
+                    if(animator.GetBool(Run))
+                        animator.SetBool(Run,false);
+                } else if(!isJumping)
+                {
+                    if (!animator.GetBool(Walk))
+                    {
+                        animator.SetBool(Walk, true);
+                        AudioManager.PlayWalkAudio();
+                    }
+
+                    if (animator.GetBool(Run))
+                    {
+                        animator.SetBool(Run, false);
+                        AudioManager.PlayWalkAudio();
+                    }
+                }
+
+
+
+            }
+        } else if (animator.GetBool(Walk) || animator.GetBool(Run))
+        {
+            AudioManager.StopPlayerAudio();
+            animator.SetBool(Walk, false);
+            animator.SetBool(Run, false);
+        }
+    }
+
+    private void ShootSequence()
+    {
+        if(Time.time<shootTime)    return;
+
+        animator.SetTrigger(animator.GetBool(Aiming) ? AimShoot : Shoot);
+        AudioManager.PlayShootAudio();
+        //AudioManager.PlayCasingAudio();
+        isShooting = true;
+        shootTime = Time.time + timeBetweenShoots;
     }
 
     private void FixedUpdate()
@@ -55,27 +173,33 @@ public class FPController : MonoBehaviour
 
     private void MovementHandler()
     {
-        float x = Input.GetAxis("Horizontal") * moveSpeed;
-        float z = Input.GetAxis("Vertical") * moveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift) && !isJumping && !isShooting ) //if runninig animation need to disable while jump add !iJumping to this line
+        {
+            moveSpeed = sprintSpeed;
+            isRunning = true;
+        } else
+        {
+            moveSpeed = walkSpeed;
+            isRunning = false;
+        }
+         x = Input.GetAxis("Horizontal") * moveSpeed;
+         z = Input.GetAxis("Vertical") * moveSpeed;
 
         transform.position += mainCamera.transform.forward * z + mainCamera.transform.right * x;
 
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rigidbody.AddForce(0, jumpForce, 0);
+            AudioManager.PlayJumpAudio();
+            isJumping = true;
         }
     }
 
     private bool IsGrounded()
     {
         RaycastHit hitInfo;
-        if (Physics.SphereCast(transform.position, capsuleCollider.radius, Vector3.down, out hitInfo,
-            (capsuleCollider.height * 0.5f) - capsuleCollider.radius + 0.1f))
-        {
-            return true;
-        }
-
-        return false;
+        return Physics.SphereCast(transform.position, capsuleCollider.radius, Vector3.down, out hitInfo,
+            (capsuleCollider.height * 0.5f) - capsuleCollider.radius + 0.01f);
     }
 
     private Quaternion ClampRotationAroundXAxis(Quaternion quaternion)
@@ -137,4 +261,6 @@ public class FPController : MonoBehaviour
             Cursor.visible = true;
         }
     }
+
+
 }
